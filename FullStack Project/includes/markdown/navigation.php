ea70d15fd16a1c02836a0aa4891662dcc43cc078
zhenpanda <zhentzhou@gmail.com>
2016-08-24 17:08:45 -0400

    navBar

---
+++ FullStack Project/includes/markdown/navigation.php
@@ -0,0 +1,39 @@
+<div id="navBarBG">
+    <div id="navBar" class="container-fluid">
+      <div class="row-fluid">
+        <div class="col-xs-6 no-gut">
+          <div id="leftImg">
+            <img src="assets/imgs/logo.png" alt="" />
+          </div>
+        </div>
+        <div class="col-xs-6 no-gut">
+              <div id="call">
+                <p id="contact">Call Us: <span id="phone-num">603-742-2424</span></p>
+              </div>
+            <div id="navBtns">
+              <button type="button" class="btn" id="patient-btn">PATIENT PORTAL</button>
+              <button type="button" class="btn" id="appointment-btn">MAKE AN APPOINTMENT</button>
+            </div>
+        </div>
+      </div>
+      <div class="row-fluid">
+        <div class="col-xs-12 no-gut">
+          <div id="navTop"></div>
+          <div id="navBarList">
+            <ul>
+              <a href="#"><li class="link">HOME</li></a>
+              <a href="#"><li class="link">OUR PROVIDERS</li></a>
+              <a href="#"><li class="link">TREATMENT & CARE</li></a>
+              <a href="#"><li class="link">RESOURCES</li></a>
+              <a href="#"><li class="link">BLOG</li></a>
+              <a href="#"><li class="link">LOCATIONS</li></a>
+              <a href="#"><li class="link">CONTACT US</li></a>
+            </ul>
+          </div>
+          <div>
+            <img id="hero-img" src="assets/imgs/hero-bg.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+</div>
--- FullStack Project/index.php
+++ FullStack Project/index.php
@@ -3,10 +3,13 @@
   <head>
     <meta charset="utf-8">
     <title></title>
-    <!-- Header -->
+    <!-- Header & dependencies -->
     <?php include "includes/header.php"; ?>
   </head>
   <body>
-    <?php include "markdown/banner.php"; ?>
+    <!-- Banner -->
+    <?php include "includes/markdown/banner.php"; ?>
+    <!-- NavBar with hero Img -->
+    <?php include "includes/markdown/navigation.php"; ?>
   </body>
 </html>
