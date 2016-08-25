9156f222bf45add2013b6e1ab2707381be08b172
zhenpanda <zhentzhou@gmail.com>
2016-08-25 00:07:18 -0400

    input

--- FullStack Project/includes/markdown/form.php
+++ FullStack Project/includes/markdown/form.php
@@ -4,8 +4,34 @@
         <div class="col-xs-12 no-gut" id="form-left">
           <p class="form-text">Are you a current patient of Sunshine Group?</p>
           <form id="paitent-form">
-            <input type="radio" value="Yes"> Yes
-            <input type="radio" value="No"> No
+            <input type="radio" name="radio-form" value="Yes"> Yes
+            <input type="radio" name="radio-form" value="No"> No
+          </form>
+          <form id="">
+            <input type="input-text" class="input" id="first-name" required="required" />
+            <label for="first-name" id="first-name-placeholder">First Name </label>
+            <input type="input-text"  class="input" id="last-name" required="required" />
+            <label for="last-name" id="last-name-placeholder">Last Name </label>
+            <input type="input-text"  class="input" id="email" required="required" />
+            <label for="email" id="email-placeholder">Email </label>
+            <input type="input-text"  class="input" id="phone" required="required" />
+            <label for="phone" id="phone-placeholder">Phone </label>
+
+            <select id="subject" class="input form-control">
+              <option value="one">Subject *</option>
+              <option value="two">Two</option>
+            </select>
+
+            <textarea type="input-text" class="input" id="comments-area" placeholder="Comments *"></textarea>
+
+            <select id="heard" class="input form-control">
+              <option value="one">How did you heard about us? *</option>
+              <option value="two">Two</option>
+            </select>
+
+            <input type="input-text"  class="input" id="details" required="required" />
+            <label for="details" id="details-placeholder">Additional referral details </label>
+
           </form>
         </div>
       </div>
